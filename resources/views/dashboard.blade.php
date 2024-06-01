ab8b0e4a8d4ebca91e13e6ee7398d8a0d4cb5dba
ouissale <www.ouissaleaitaissa@gmail.com>
2024-06-01 02:38:22 +0200

    33 commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dasjjjjjjjjhboard') }}
+            {{ __('Dashboard') }}
         </h2>
-    </x-slot>
+    </x-slot>git
+    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
