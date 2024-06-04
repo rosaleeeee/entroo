44eefd53ef30705f0d39b0ace24888f4a839c42a
zaynab <zinebaboutalib90@gmail.com>
2024-06-04 15:40:05 +0100

    z1

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,79 +11,7 @@
     <link href="{{}a}"
     <!-- Styles -->
     <style>
-        * {
-            margin: 0;
-            padding: 0;
-            outline: none;
-            border: none;
-            text-decoration: none;
-            box-sizing: border-box;
-            font-family: "Poppins", sans-serif;
-        }
-
        
-
-        .nav1 {
-            
-            position:absolute;
-            top:0;
-            background-color: red;
-            bottom: 0;
-            height:100%;
-            left:0;
-            background: #fff;
-            width: 90px;
-            overflow: hidden;
-            transition: width 0.2s linear;
-            box-shadow:0 20px 35px rgba(0,0,0,0.1);
-        }
-        .logo{
-            text-align:-50px;
-            display: flex;
-            transition: all 0.5s ease;
-            margin: 80px  0 0 5px;
-            
-    
-
-        }
-        .logo img{
-            width: 5; 
-            height: 5; 
-            border-radius: 50%;
-
-        }
-        .logo span{
-            font-weight: bold;
-            padding-left:15px ;
-            font-size: 18px;
-            text-transform: uppercase;
-
-        }
-        a{
-            position: relative;
-            color:rgb(85,83,83);
-            font-size: 14px;
-            display: table;
-            width: 300px;
-            padding: 10px;
-        }
-        .fas{
-            position: relative;
-            width:70px;
-            height: 40px;
-            top: 14px;
-            font-size: 20px;
-            text-align:center ;
-        }
-        .nav-item{
-            position: relative;
-            top: 12px;
-            margin-left: 10px;
-        }
-        a:hover{
-            background: #eee;
-        }
-        
     </style>
 </head>
 <body>
