04fbb929254d200677157aab9dfbcb8745b3a537
ouissale <www.ouissaleaitaissa@gmail.com>
2024-06-05 15:48:44 +0200

    w1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,22 +12,27 @@
     </div>
     
     <div class="levels">
-  <div class="py-6">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 flex items-center justify-between">
-          <div class="flex items-center">
-            <img src="Images/L1.png" class="w-12 h-12 mr-4" alt="Image 1">
-            <p>{{ __("Business Model") }}</p>
-          </div>
-          <img src="image2.jpg" class="w-12 h-12" alt="Image 2">
+      <div id="level1" class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg level_pointer">
+                <div class="p-6 flex items-center justify-between">
+                    <div class="flex items-center">
+                        <img src="Images/L1.png" class="w-12 h-12 mr-4" alt="Image 1">
+                        <p>{{ __("Business Model") }}</p>
+                    </div>
+                    <img src="image2.jpg" class="w-12 h-12" alt="Image 2">
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <div class="py-6">
+  <script>
+      document.getElementById('level1').onclick = function() {
+          window.location.href = '{{ route("level1") }}';
+      };
+  </script>  
+  <div id="level2" class="py-6">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg level_pointer">
         <div class="p-6 flex items-center justify-between">
           <div class="flex items-center">
             <img src="Images/L2.png" class="w-12 h-12 mr-4" alt="Image 1">
@@ -38,9 +43,14 @@
       </div>
     </div>
   </div>
-  <div class="py-6">
+  <script>
+    document.getElementById('level2').onclick = function() {
+        window.location.href = '{{ route("level2") }}';
+    };
+</script>
+  <div id="level3" class="py-6">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg level_pointer">
         <div class="p-6 flex items-center justify-between">
           <div class="flex items-center">
             <img src="Images/L3.png" class="w-12 h-12 mr-4" alt="Image 1">
@@ -51,9 +61,14 @@
       </div>
     </div>
   </div>
-  <div class="py-6">
+  <script>
+    document.getElementById('level3').onclick = function() {
+        window.location.href = '{{ route("level3") }}';
+    };
+</script>
+  <div id="level4" class="py-6">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg level_pointer">
         <div class="p-6 flex items-center justify-between">
           <div class="flex items-center">
             <img src="Images/L4.png" class="w-12 h-12 mr-4" alt="Image 1">
@@ -64,5 +79,10 @@
       </div>
     </div>
   </div>
+  <script>
+    document.getElementById('level4').onclick = function() {
+        window.location.href = '{{ route("level4") }}';
+    };
+</script>
 </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/level1', function () {
+    return view('level1.level1_obj');
+})->name('level1');
+
+Route::get('/level2', function () {
+    return view('level2.level2_obj');
+})->name('level2');
+
+Route::get('/level3', function () {
+    return view('level3.level3_obj');
+})->name('level3');
+
+Route::get('/level4', function () {
+    return view('level4.level4_obj');
+})->name('level4');
 
 require __DIR__.'/auth.php';
