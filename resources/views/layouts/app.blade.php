2938597dda3caf4cadfa834cda8a3d03d1d1960c
ouissale <www.ouissaleaitaissa@gmail.com>
2024-06-10 21:10:41 +0200

    w12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{s{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>EntroSkills</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/level4/quiz/result.blade.php
+++ resources/views/level4/quiz/result.blade.php
@@ -1,3 +1,4 @@
+<x-app-layout>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +13,11 @@
     @include('layouts.sidebar')
     <div class="container_result">
     <h1>Your MBTI Type is: {{ $mbti_type }}</h1>
-    <div class="pie" style="--p:70"> 20%</div>
-    <div class="pie" style="--p:40;--c:darkblue;--b:10px"> 40%</div>
-    <div class="pie no-round" style="--p:60;--c:purple;--b:15px"> 60%</div>
     <div class="pie animate no-round" style="--p:80;--c:orange;"> 80%</div>
-    <div class="pie animate" style="--p:90;--c:lightgreen"> 90%</div>
+    <div class="pie animate no-round" style="--p:80;--c:orange;"> 80%</div>
+    <div class="pie animate no-round" style="--p:80;--c:orange;"> 80%</div>
+    <div class="pie animate no-round" style="--p:80;--c:orange;"> 80%</div>
+    <h2>Your MBTI Type is: {{ $mbti_type }}</h2>
     <h1>Quiz Results</h1>
     <p>E: {{ $results_E }}</p>
     <p>I: {{ $results_I }}</p>
@@ -26,7 +27,8 @@
     <p>F: {{ $results_F }}</p>
     <p>J: {{ $results_J }}</p>
     <p>P: {{ $results_P }}</p>    
-    <h2>Your MBTI Type is: {{ $mbti_type }}</h2>
     </div>
 </body>
 </html>
+</x-app-layout>
+
