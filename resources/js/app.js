83dd7d8e9efd222dee0ff0c6fa4569fc1a95cdc1
ouissale <www.ouissaleaitaissa@gmail.com>
2024-06-16 21:21:10 +0200

    w18

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,38 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DragAndDrop from './components/DragAndDrop'; // Assurez-vous que l'extension est .jsx
+import './bootstrap';
 
-const users = [
-    { id: '1', name: 'Nawali Salma' },
-    { id: '2', name: 'Mehdaoui Mariam' },
-    { id: '3', name: 'Housam Abdellatif' },
-    { id: '4', name: 'Khalil Amin' },
-    { id: '5', name: 'Zouhir Rania' },
-    { id: '6', name: 'Bensalem Fares' },
-    { id: '7', name: 'Tazi Yassine' },
-    { id: '8', name: 'Khalfi Nadia' },
-    { id: '9', name: 'Saidi Karim' },
-];
+import Alpine from 'alpinejs';
 
-const jobs = [
-    { id: 'job-1', title: 'Product Manager', assignedUser: null },
-    { id: 'job-2', title: 'Sales Manager', assignedUser: null },
-    { id: 'job-3', title: 'Accountant', assignedUser: null },
-    { id: 'job-4', title: 'Marketing Manager', assignedUser: null },
-    { id: 'job-5', title: 'Partnerships Manager', assignedUser: null },
-    { id: 'job-6', title: 'Chief Operating Officer (COO)', assignedUser: null },
-    { id: 'job-7', title: 'Chief Financial Officer (CFO)', assignedUser: null },
-    { id: 'job-8', title: 'Project Manager', assignedUser: null },
-    { id: 'job-9', title: 'Customer Service Manager', assignedUser: null },
-];
+window.Alpine = Alpine;
 
-function App() {
-    return (
-        <div className="container">
-            <h1>Drag and Drop Example</h1>
-            <DragAndDrop users={users} jobs={jobs} />
-        </div>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+Alpine.start();
