8d6e3843cf22982c593057df09bf46b881e2d398
ouissale <www.ouissaleaitaissa@gmail.com>
2024-06-21 18:09:22 +0200

    w25

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,27 @@
             width: 150px;
             height: auto;
         }
+        #menu a{
+            text-decoration:none;   
+            color: black;
+        }
         #menu .buttons button {
             color: black;
-            text-decoration: none;
-            padding: 8px 20px;
+            text-decoration:none;
+            width: 120px;
+            height: 50px;
             margin: 10px;
-            border-radius: 5px;
-            background-color: #FED101;
+            background-color: #ffd700;
+            border-radius:  30px;
+            align-items: center;
+            cursor: pointer;
+            font-weight: bold;
+            border: 1px;
+            font-size: 15px;
+        }
+        
+        #menu .buttons button:hover {
+            background-color: #ffcc00;
         }
 
 
@@ -45,18 +59,53 @@
         }
         #left h1 {
             color: black;
-            font-size: 60px;
+            font-size: 70px;
+            margin-bottom: 10px;
+            font-weight: bold;
         }
         #left h3 {
             color:#13538A;
-            font-size: 20px; 
+            font-size: 30px; 
+            font-weight: bold;
 
         }
+        .typewriter h3 {
+            overflow: hidden; /* Ensures the content is not revealed until the animation */
+            border-right: .15em solid orange; /* The typwriter cursor */
+            white-space: nowrap; /* Keeps the content on a single line */
+            margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+            letter-spacing: .10em; /* Adjust as needed */
+            animation: 
+                typing 2.5s steps(40, end),
+                blink-caret .75s step-end infinite;
+            }
+
+            /* The typing effect */
+            @keyframes typing {
+            from { width: 0 }
+            to { width: 100% }
+            }
+
+            /* The typewriter cursor effect */
+            @keyframes blink-caret {
+            from, to { border-color: transparent }
+            50% { border-color: #13538A; }
+            }
+        #left p {
+            color:black;
+            font-size: 22px; 
+            text-align: justify;
+
+        }
+        
         #right img {
             max-width: 100%;
+            width: 650px;
             height: auto;
+            margin: 100px;
             margin-right : 100px;
         }
+
     </style>
 </head>
 <body>
@@ -72,13 +121,18 @@
     <div id="content">
         <div id="left">
             <h1>EntroSkills</h1>
-            <h3>Ignite innovation, elevate entrepreneurship.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="typewriter">
+            <h3>Empower Your Entrepreneurial Journey</h3>
+             </div>
+            <p >Unlock your entrepreneurial potential! Start an exciting journey 
+                to build and refine your business skills. Through fun exercises 
+                and expert tips, you'll learn to create successful business models 
+                and bring your ideas to life. Begin your path to success today!</p>
+           
         </div>
         <div id="right">
             <img src="{{ asset('images/Home.png') }}" alt="Illustration">
         </div>
     </div>
 </body>
-</html>
 </html>
